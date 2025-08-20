dfbb0241ad807dbb3af0ad1185754d7fbbe40f6e
KYH1013 <yunha081013@gmail.com>
2025-08-20 11:55:15 +0900

    Update 수행.py

--- 수행.py
+++ 수행.py
@@ -25,7 +25,7 @@ if dob:
         st.write(f"💎 **탄생석:** {stone['name']} - {stone['meaning']}")
 
     # 별자리
-    sign, sign_emoji = birthdata.ZODIAC_SIGNS(month, day)
+    sign, sign_emoji = birthdata.ZODIAC_SIGNS.get(month, day)
     st.write(f"✨ **별자리:** {sign} {sign_emoji}")
 
     # 띠
