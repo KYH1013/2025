f109c18f87114e1ed605350bdb05cc12f6a53f10
KYH1013 <yunha081013@gmail.com>
2025-08-23 15:25:19 +0900

    Update 수행.py

--- 수행.py
+++ 수행.py
@@ -15,8 +15,8 @@ if dob:
     st.subheader(f"🎂 {dob.strftime('%Y년 %m월 %d일')} 정보")
 
    month_day_key = f"{month:02d}-{day:02d}"   # 예: 01-01
-flower = birthdata.BIRTH_FLOWERS_BY_DAY.get(month_day_key)
-if flower:
+    flower = birthdata.BIRTH_FLOWERS_BY_DAY.get(month_day_key)
+    if flower:
     st.write(f"🌸 **탄생화:** {flower['name']} - {flower['meaning']}")
 
     # 탄생석
