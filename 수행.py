9785d3c7696847c23ad143cec6aefffc79575f11
KYH1013 <yunha081013@gmail.com>
2025-08-26 23:29:27 +0900

    Update 수행.py

--- 수행.py
+++ 수행.py
@@ -67,12 +67,12 @@ with tab1:
         sign, sign_emoji = birthdata.get_zodiac(month1, day1)
         with st.expander("✨ 별자리"):
             st.write(f"{sign} {sign_emoji}")
-
-# 띠 & 궁합
-        chinese_zodiac = birthdata.get_chinese_zodiac(year1)
-        compat = ZODIAC_COMPATIBILITY.get(chinese_zodiac, {"좋음": [], "안좋음": []})
+    with col2:
+        # 띠 & 궁합
         with st.expander("🐲 띠 & 궁합"):
-            st.write(f"{chinese_zodiac}\n💖 {', '.join(compat['좋음'])}\n💔 {', '.join(compat['안좋음'])}")
+        chinese_zodiac = birthdata.get_chinese_zodiac(dob1.year)  # 매번 dob1.year 기준
+        compat = ZODIAC_COMPATIBILITY.get(chinese_zodiac, {"좋음": [], "안좋음": []})
+        st.write(f"{chinese_zodiac}\n💖 {', '.join(compat['좋음'])}\n💔 {', '.join(compat['안좋음'])}")
 
 
     # 월별 기념일
